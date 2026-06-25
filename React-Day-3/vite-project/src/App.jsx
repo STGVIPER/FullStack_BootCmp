80c6ad687cb4d01a99b3ee4974f54345ba2f5f02
Kunal Rai <kunalrai2005@gmail.com>
2026-06-25 14:12:44 +0530

    react d3

---
+++ React-Day-3/vite-project/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import UseEffectOne from './UseEffectOne'
+import Timer from './Timer'
+import WindowResize from './WindowResize'
+import Routing from './Routing'
+import FlowingMenu from './FlowingMenu'
+
+const App = () => {
+  
+  return (
+    <div>
+      {/* <UseEffectOne/> */}
+      {/* <Timer/> */}
+      {/* <WindowResize/> */}
+      <Routing/>
+    </div>
+  )
+}
+
+export default App
